a82aa78504843de8f1f01cdcecc969f97aba3dbb
sto <Son.C.To@gmail.com>
2015-07-01 08:22:26 -0400

    initial commit

---
+++ app/src/main/java/com/happy/happywidget/HappyWidgetProvider.java
@@ -0,0 +1,80 @@
+package com.happy.happywidget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+/**
+ * Created by sto on 7/1/15.
+ */
+public class HappyWidgetProvider extends AppWidgetProvider {
+    private static final String MyOnClick1 = "myOnClickTag";
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+
+        for (int i = 0; i < N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.hellowidget_layout);
+
+            remoteViews.setOnClickPendingIntent(R.id.button_send, getPendingSelfIntent(context, MyOnClick1));
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+        }
+    }
+
+    protected PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, getClass());
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        Toast.makeText(context, "Button1", Toast.LENGTH_SHORT).show();
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("http://172.16.0.34:3000/happinesses");
+
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("POST");
+                    conn.setDoInput(true);
+                    OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+
+                    writer.write("username=foo%password=bar");
+                    writer.flush();
+
+                    String line;
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                    while ((line = reader.readLine()) != null) {
+                        Log.d("Foo", line);
+                    }
+                    writer.close();
+                    reader.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+
+            }
+        };
+        t.start();
+    }
+}
